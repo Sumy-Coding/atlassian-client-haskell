68a3a7dff020db380ef59e8843ae0e02d3ec7544
andrii <quadr988@gmail.com>
2023-06-12 18:11:16 +0300

    Getting issue, TOTOs

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,42 @@
 module Main where
 
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 import           Data.Aeson            (Value)
 import qualified Data.ByteString.Char8 as S8
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.Yaml             as Yaml
 import           Network.HTTP.Client
+import GHC.Generics
 
+
+data Project = Project {
+        pSelf :: String
+      , pId :: Int
+      , pKey :: String
+      , pName :: String 
+      , projectTypeKey :: String } deriving (Generic, Show)
+
+data Fields = Fields {
+        fTimespent :: Maybe String
+      , fProject :: Project
+      , fAssignee :: Maybe String } deriving (Generic, Show)
+
+data Issue = Issue {
+        iId :: String
+      , issSelf :: String
+      , iKey :: String
+      , iFields :: Fields  } deriving (Generic, Show)
+
+instance Yaml.FromJSON Issue
+instance Yaml.ToJSON Issue
+
+instance Yaml.FromJSON Fields
+instance Yaml.ToJSON Fields
+
+instance Yaml.FromJSON Project
+instance Yaml.ToJSON Project
 
 main :: IO ()
 main = do
@@ -26,7 +55,12 @@ main = do
                show (responseStatus response)
     
     let headers = responseHeaders response
-
-    L8.putStrLn $ responseBody response
+    let respBody = responseBody response
+    
+    S8.putStrLn $ Yaml.decode (respBody)
 
     responseClose response
+
+    
+
+
